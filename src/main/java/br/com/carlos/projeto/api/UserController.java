f0d983c71a44e0d103d0bf3d86a59739cd014b59
Carlos Eduardo <carloseduscc@gmail.com>
2025-11-04 22:11:16 -0300

    feat(User): Implementa busca de usuário por ID

--- src/main/java/br/com/carlos/projeto/api/UserController.java
+++ src/main/java/br/com/carlos/projeto/api/UserController.java
@@ -3,17 +3,16 @@ package br.com.carlos.projeto.api;
 import br.com.carlos.projeto.application.UserService;
 import br.com.carlos.projeto.application.command.RegisterProfessionalProfileCommand;
 import br.com.carlos.projeto.application.dto.ProfessionalProfileDTO;
+import br.com.carlos.projeto.application.dto.UserDTO;
+import br.com.carlos.projeto.domain.User;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping
+@RequestMapping("/users")
 @Tag(name = "Ações de usuário")
 public class UserController {
 
@@ -27,6 +26,13 @@ public class UserController {
     @PostMapping("/professional-profile")
     public ResponseEntity<ProfessionalProfileDTO> registerProfessionalProfile(@RequestBody @Valid RegisterProfessionalProfileCommand cmd){
         ProfessionalProfileDTO response = userService.registerProfessionalProfile(cmd);
+        return ResponseEntity.created(null).body(response);
+    }
+
+    @Operation(summary = "Busca um usuário pelo ID")
+    @GetMapping("/{id}")
+    public ResponseEntity<UserDTO> findById(@PathVariable Long id){
+        UserDTO response = userService.findById(id);
         return ResponseEntity.ok(response);
     }
 }
--- src/main/java/br/com/carlos/projeto/application/UserService.java
+++ src/main/java/br/com/carlos/projeto/application/UserService.java
@@ -2,6 +2,7 @@ package br.com.carlos.projeto.application;
 
 import br.com.carlos.projeto.application.command.RegisterProfessionalProfileCommand;
 import br.com.carlos.projeto.application.dto.ProfessionalProfileDTO;
+import br.com.carlos.projeto.application.dto.UserDTO;
 import br.com.carlos.projeto.application.mapper.ProfessionalProfileMapper;
 import br.com.carlos.projeto.application.mapper.UserMapper;
 import br.com.carlos.projeto.domain.ProfessionalProfile;
@@ -10,6 +11,8 @@ import br.com.carlos.projeto.domain.repository.UserRepository;
 import br.com.carlos.projeto.infra.persistence.entity.UserEntity;
 import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
+
+import java.util.NoSuchElementException;
 
 @Service
 public class UserService {
@@ -39,4 +42,16 @@ public class UserService {
 
         return pMapper.toDTO(user.getProfessionalProfile());
     }
+
+    @Transactional
+    public UserDTO findById(Long id){
+        try {
+            return uMapper.toDTO(uMapper.fromEntity(repo.findById(id)));
+
+        } catch (NoSuchElementException ex){
+            throw new NoSuchElementException("User not found with id: " + id);
+        }
+
+
+        }
 }
